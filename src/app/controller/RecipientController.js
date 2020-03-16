33bc4ba155b92abe34678589b81443d2c609bdeb
gyasmin <gyasminassuncao@gmail.com>
2020-03-16 11:15:33 -0300

    Criando middleware que verifica se o usuário tentando criar ou alterar os destinatários é um user admin

--- src/app/controller/RecipientController.js
+++ src/app/controller/RecipientController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipients';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      cidade: Yup.string().required(),
+      estado: Yup.string().required(),
+      cep: Yup.string()
+        .required()
+        .max(8),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
     const recipientExists = await Recipient.findOne({
       where: { name: req.body.name } && { numero: req.body.numero },
     });
@@ -18,7 +32,29 @@ class RecipientController {
     if (!recipient) {
       return res.status(401).json({ error: 'Recipient not found' });
     }
-    return res.json(recipient);
+
+    const {
+      name,
+      rua,
+      numero,
+      complemento,
+      cidade,
+      estado,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      endereço: {
+        rua,
+        numero,
+        complemento,
+        cidade,
+        estado,
+        cep,
+      },
+    });
   }
 }
 export default new RecipientController();
