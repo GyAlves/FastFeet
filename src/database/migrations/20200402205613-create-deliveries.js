748d134d3272e30129a35d999415663630428c25
gyasmin <gyasminassuncao@gmail.com>
2020-04-02 18:13:44 -0300

    Criando a migration e o model das entregas

---
+++ src/database/migrations/20200402205613-create-deliveries.js
@@ -0,0 +1,55 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliveries', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'recipients', key: 'id' },
+        allowNull: false,
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'deliverymans', key: 'id' },
+        allowNull: false,
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        allowNull: true,
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('deliveries');
+  },
+};
